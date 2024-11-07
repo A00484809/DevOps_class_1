2699ad18c8e017fe9855ca1437d2daaf2b8788c2
Library <Library@LIB-LAPTOP31.SMUNET.SMU.CA>
2024-11-06 20:01:37 -0400

    new

--- src/DevOps_class_1.Server/obj/Release/net8.0/DevOps_class_1.Server.AssemblyInfo.cs
+++ src/DevOps_class_1.Server/obj/Release/net8.0/DevOps_class_1.Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevOps_class_1.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15296cf8400325b00adaea0ab181a7c514629ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevOps_class_1.Server")]
